fc3845ed59b81ac92f1d210f7ae48a48399f5fcc
vedantnilabh <vedant.nilabh@gmail.com>
2023-04-19 03:00:39 -0500

    increased efficiency to optimal

--- hw3.cpp
+++ hw3.cpp
@@ -10,7 +10,7 @@ bool is_better(const pair<float, float>& a, const pair<float, float>& b) {
 }
 
 // Define the recursive function to find the best applicants
-vector<pair<float, float>> find_best_applicants(vector<pair<float, float>>& applicants) {
+vector<pair<float, float>> find_best_applicants(const vector<pair<float, float>>& applicants) {
     // Base case: if there's only one applicant, return it
     if (applicants.size() == 1) {
         return applicants;
@@ -27,33 +27,30 @@ vector<pair<float, float>> find_best_applicants(vector<pair<float, float>>& appl
 
     // Combine the results of the two sets to form the final solution
     vector<pair<float, float>> best_applicants;
-    for (const auto& a : left_best) {
-        if (all_of(right_best.begin(), right_best.end(), [&](const pair<float, float>& b) {
-            return !is_better(b, a);
-        })) {
-            best_applicants.push_back(a);
-        }
-    }
-    for (const auto& b : right_best) {
-        if (all_of(left_best.begin(), left_best.end(), [&](const pair<float, float>& a) {
-            return !is_better(a, b);
-        })) {
-            best_applicants.push_back(b);
+    merge(left_best.begin(), left_best.end(), right_best.begin(), right_best.end(), back_inserter(best_applicants), [&](const pair<float, float>& a, const pair<float, float>& b) {
+        return is_better(a, b);
+    });
+
+    // Remove applicants that are not eligible
+    vector<pair<float, float>> eligible_applicants;
+    for (const auto& a : best_applicants) {
+        bool is_eligible = all_of(applicants.begin(), applicants.end(), [&](const pair<float, float>& b) {
+            return (&a != &b) && !is_better(b, a);
+        });
+        if (is_eligible) {
+            eligible_applicants.push_back(a);
         }
     }
 
-    return best_applicants;
+    return eligible_applicants;
 }
 
 // Define the public function that calls the recursive function and returns a vector of indices of the best applicants
 vector<int> BestApplicants(const vector<pair<float, float>>& applicants) {
-    vector<pair<float, float>> applicant_scores;
-    for (int i = 0; i < applicants.size(); i++) {
-        applicant_scores.push_back(applicants[i]);
-    }
+    // Find the best applicants
+    vector<pair<float, float>> best_applicant_scores = find_best_applicants(applicants);
 
-    vector<pair<float, float>> best_applicant_scores = find_best_applicants(applicant_scores);
-
+    // Create a vector of indices of the best applicants
     vector<int> best_applicants;
     for (int i = 0; i < applicants.size(); i++) {
         if (find(best_applicant_scores.begin(), best_applicant_scores.end(), applicants[i]) != best_applicant_scores.end()) {
